4ff4f11a8e8d232ff807ed82b7676758d72f1da1
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-07 22:12:34 -0600

    inject Bower CSS and Bower JavaScript into HTML

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,11 @@ gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
-    }
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(gulp.dest('./src/views'));
-})
+});
