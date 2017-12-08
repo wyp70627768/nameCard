bc3a707f093c003259b6479d9220298065c12b6b
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-07 20:06:08 -0600

    use middleware app.use to include static files into project

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res) {
 
 });
