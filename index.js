8f3263aba476c7fb759b35f8a561fffe73cee331
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-08 00:14:08 -0600

    installed ejs and use ejs substitute for html

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var port = 3000 || process.env.PORT;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+
+// var handlebars = require('express-handlebars')
+  // app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {title:'Hello from render', list: ['a', 'b']});
 });
 
 app.listen(port, function(err) {
