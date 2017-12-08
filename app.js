dbba065f2dc70276d189aa25a207200324a0baeb
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-08 11:40:40 -0600

    use mssql to update books and use middleware in the code

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
+var sql = require('mssql');
+var config = {
+    user: 'books',
+    password: 'pluralsight1@',
+    server: 'gpnju6fwr2.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'Books',
+
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+};
+
+sql.connect(config, function (err) {
+    console.log(err);
+});
+
 var port = process.env.PORT || 5000;
 var nav = [{
     Link: '/Books',
     Text: 'Book'
-    }, {
+}, {
     Link: '/Authors',
     Text: 'Author'
-    }];
+}];
+
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
@@ -18,16 +35,16 @@ app.set('view engine', 'ejs');
 app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
-  res.render('index', {
-      title: 'Hello from render',
-      nav: [{
-          Link: '/Books',
-          Text: 'Books'
-      }, {
-          Link: '/Authors',
-          Text: 'Authors'
-      }]
-  });
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function (req, res) {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,77 +1,103 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var sql = require('mssql');
 
-var router = function(nav){
+var router = function (nav) {
     var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
         },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false
+        {
+            title: 'Les Misérables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
         },
-    {
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: false
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H. G. Wells',
+            read: false
         },
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        author: 'Jules Verne',
-        read: false
+        {
+            title: 'A Journey into the Center of the Earth',
+            genre: 'Science Fiction',
+            author: 'Jules Verne',
+            read: false
         },
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false
+        {
+            title: 'The Dark World',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false
         },
-    {
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        author: 'Kenneth Grahame',
-        read: false
+        {
+            title: 'The Wind in the Willows',
+            genre: 'Fantasy',
+            author: 'Kenneth Grahame',
+            read: false
         },
-    {
-        title: 'Life On The Mississippi',
-        genre: 'History',
-        author: 'Mark Twain',
-        read: false
+        {
+            title: 'Life On The Mississippi',
+            genre: 'History',
+            author: 'Mark Twain',
+            read: false
         },
-    {
-        title: 'Childhood',
-        genre: 'Biography',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
+        {
+            title: 'Childhood',
+            genre: 'Biography',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
         }
     ];
     bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('bookListView', {
-            title: 'Books',
-            nav: nav,
-            books: books
+        .get(function (req, res) {
+            var request = new sql.Request();
+
+            request.query('select * from books',
+                function (err, recordset) {
+                    res.render('bookListView', {
+                        title: 'Books',
+                        nav: nav,
+                        books: recordset
+                    });
+                });
+
         });
-    });
 
     bookRouter.route('/:id')
-    .get(function (req, res) {
-        var id = req.params.id;
-        res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: books[id]
+        .all(function (req, res, next) {
+            var id = req.params.id;
+            var ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int);
+            ps.prepare('select * from books where id = @id',
+                function (err) {
+                    ps.execute({
+                            id: req.params.id
+                        },
+                        function (err, recordset) {
+                            if (recordset.length === 0) {
+                                res.status(404).send('Not Found');
+                            } else {
+                                req.book = recordset[0];
+                                next();
+                            }
+
+                        });
+                });
+        })
+        .get(function (req, res) {
+            res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: req.book
+            });
+
         });
-    });
-    
+
     return bookRouter;
 };
 module.exports = router;
