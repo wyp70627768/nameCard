ee7966e47892c56dc3574af4497d1570a88cd80d
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-08 11:05:20 -0600

    create router for books and make the code clean and also nav

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var nav = [{
+    Link: '/Books',
+    Text: 'Book'
+    }, {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
-    res.render('index', {
+  res.render('index', {
       title: 'Hello from render',
       nav: [{
           Link: '/Books',
           Text: 'Books'
-            },{
+      }, {
           Link: '/Authors',
           Text: 'Authors'
-            }
-      ]
-    });
+      }]
+  });
 });
 
 app.get('/books', function (req, res) {
